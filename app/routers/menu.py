d60cbbd8b43dfe82187d3732ff07da40bb849732
Mir Mazedur Rahaman <mir.rahaman@bareways.com>
2023-11-20 00:25:46 +0100

    menu item fixed.

--- app/routers/menu.py
+++ app/routers/menu.py
@@ -1,11 +1,17 @@
 from typing import Optional
 
 import httpx
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 
 from app import templates
-from app.services.menu import get_menu_form_creds
+from app.services.menu import (
+    get_menu_form_creds,
+    get_menu_item,
+    add_menu_item,
+    update_menu_item,
+    delete_menu_item,
+)
 from app.utils.auth import User, UserSession, get_user_session, get_userinfo_for_page
 from app.utils.exceptions import UnauthorizedPageException
 from config.settings import settings
@@ -59,6 +65,42 @@ async def get_add_menu_page(
     return templates.TemplateResponse("pages/menu_add.html", context)
 
 
+# View menu item page
+@router.get(
+    path="/view/{menu_id}",
+    summary="Gets the menu item view page.",
+    tags=["Pages"],
+    response_class=RedirectResponse,
+)
+async def get_view_menu_page(
+    menu_id: int,
+    request: Request,
+    session: Optional[UserSession] = Depends(get_user_session),
+) -> HTMLResponse:
+    item = await get_menu_item(menu_id=menu_id, user_session=session.user_session)
+    title = "View Menu"
+    context = {"request": request, "item": item, "title": title, "nav_menu": "true"}
+    return templates.TemplateResponse("pages/menu_view.html", context)
+
+
+# Edit menu item page
+@router.get(
+    path="/edit/{menu_id}",
+    summary="Gets the menu item edit page.",
+    tags=["Pages"],
+    response_class=RedirectResponse,
+)
+async def get_edit_menu_page(
+    menu_id: int,
+    request: Request,
+    session: Optional[UserSession] = Depends(get_user_session),
+) -> HTMLResponse:
+    item = await get_menu_item(menu_id=menu_id, user_session=session.user_session)
+    title = "Edit Menu"
+    context = {"request": request, "item": item, "title": title, "nav_menu": "true"}
+    return templates.TemplateResponse("pages/menu_edit.html", context)
+
+
 # Menu data router
 # Get menu data
 @router.get(
@@ -86,19 +128,55 @@ async def get_menu_data(
     return templates.TemplateResponse("partials/menu/menu_list.html", context)
 
 
-# Add menu data
+# Add menu item
 @router.post(
-    path="/data",
-    summary="Adds the menu data.",
+    path="/data/add",
+    summary="Adds the menu item.",
     tags=["Pages"],
-    response_class=HTMLResponse,
+    response_class=RedirectResponse,
 )
 async def add_menu_data(
-    status_code: int = Depends(get_menu_form_creds),
-) -> HTMLResponse:
-    # if status_code != 201:
-    #     raise UnauthorizedPageException()
-    return RedirectResponse(url="/menu", status_code=303)
+    data: dict = Depends(get_menu_form_creds),
+    session: Optional[UserSession] = Depends(get_user_session),
+) -> RedirectResponse:
+    status_code = await add_menu_item(data=data, user_session=session.user_session)
+    if status_code == 200:
+        return RedirectResponse(url="/menu", status_code=303)
+
+
+# Edit menu item
+@router.post(
+    path="/data/edit/{menu_id}",
+    summary="Edits the menu item.",
+    tags=["Pages"],
+    response_class=RedirectResponse,
+)
+async def edit_menu_data(
+    menu_id: int,
+    data: dict = Depends(get_menu_form_creds),
+    session: Optional[UserSession] = Depends(get_user_session),
+) -> RedirectResponse:
+    status_code = await update_menu_item(
+        data=data, menu_id=menu_id, user_session=session.user_session
+    )
+    if status_code == 200:
+        return RedirectResponse(url="/menu", status_code=303)
+
+
+# Delete menu item
+@router.delete(
+    path="/{menu_id}",
+    summary="Deletes the menu item.",
+    tags=["Pages"],
+)
+async def delete_menu_data(
+    menu_id: int,
+    session: Optional[UserSession] = Depends(get_user_session),
+) -> None:
+    status_code = await delete_menu_item(
+        menu_id=menu_id, user_session=session.user_session
+    )
+    return Response(status_code=status_code)
 
 
 # Get item category list dropdown
--- app/services/menu.py
+++ app/services/menu.py
@@ -9,16 +9,15 @@ from config.settings import settings
 
 def get_menu_form_creds(
     name: str = Form(),
-    price: float = Form(),
-    description: str = Form(),
-    making_time: str = Form(),
+    price: int = Form(),
+    description: Optional[str] = Form(default=None),
+    making_time: int = Form(),
     spice_level: str = Form(),
     vegetarian: bool = Form(),
     vegan: bool = Form(),
     gluten_free: bool = Form(),
     status: str = Form(),
-    session: Optional[UserSession] = Depends(get_user_session),
-) -> int:
+) -> dict:
     image = "https://pngfre.com/wp-content/uploads/Burger-43.png"
     status = True if status == "Available" else False
     data = {
@@ -26,7 +25,7 @@ def get_menu_form_creds(
         "name": name,
         "description": description,
         "price": price,
-        "making_time": making_time.replace(":", "."),
+        "making_time": making_time,
         "image": str(image),
         "status": status,
         "spice_level": spice_level.lower(),
@@ -34,15 +33,75 @@ def get_menu_form_creds(
         "vegan": vegan,
         "gluten_free": gluten_free,
     }
+    return data
+
+
+async def get_menu_item(
+    menu_id: int,
+    user_session: str,
+) -> dict:
+    with httpx.Client() as client:
+        try:
+            response = client.get(
+                f"{settings.api_host}/menu",
+                headers={"Authorization": f"Bearer {user_session}"},
+                params={"menu_id": menu_id},
+            )
+            response.raise_for_status()
+            item = response.json()["menu_items"][0]
+        except httpx.HTTPError as e:
+            item = {}
+    return item
+
+
+async def add_menu_item(
+    data: dict,
+    user_session: str,
+) -> int:
     with httpx.Client() as client:
         try:
             response = client.post(
                 f"{settings.api_host}/menu",
-                headers={"Authorization": f"Bearer {session.user_session}"},
+                headers={"Authorization": f"Bearer {user_session}"},
                 json=data,
             )
             response.raise_for_status()
-        except httpx.HTTPError:
+        except httpx.HTTPError as e:
             response = None
-        print(response.status_code)
+    return response.status_code
+
+
+async def update_menu_item(
+    data: dict,
+    menu_id: int,
+    user_session: str,
+) -> int:
+    with httpx.Client() as client:
+        try:
+            data["id"] = menu_id
+            response = client.put(
+                f"{settings.api_host}/menu",
+                headers={"Authorization": f"Bearer {user_session}"},
+                json=data,
+            )
+            response.raise_for_status()
+        except httpx.HTTPError as e:
+            response = None
+    return response.status_code
+
+
+async def delete_menu_item(
+    menu_id: int,
+    user_session: str,
+) -> int:
+    with httpx.Client() as client:
+        try:
+            response = client.delete(
+                f"{settings.api_host}/menu",
+                headers={"Authorization": f"Bearer {user_session}"},
+                params={"menu_id": menu_id},
+            )
+            response.raise_for_status()
+        except httpx.HTTPError as e:
+            response = None
     return response.status_code
